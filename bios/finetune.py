cd5a05daa7e5241312cd8f9d3a3d78a33d6c726c
shauli <shauli321@gmail.com>
2022-06-02 18:03:04 +0200

    update

--- bios/finetune.py
+++ bios/finetune.py
@@ -180,9 +180,9 @@ def eval_dev(bert, W, texts_dev, y_dev, device, adv=None, y_dev_gender=None):
 
 
 pbar = tqdm.tqdm(range(args.iters), ascii=True)
-d = args.__dict__
-run = wandb.init(reinit=True, project="rlace-finetune-bios", config=d)
-wandb.config.update({"lr": lr, "momentum": momentum, "decay": decay} | d)
+d, d2 = args.__dict__, {"lr": lr, "momentum": momentum, "decay": decay}
+new_d = {**d, **d2}
+run = wandb.init(reinit=True, project="rlace-finetune-bios", config=new_d)
 
 
 for i in pbar:
