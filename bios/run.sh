46682582384046a49ef95dbb11cf9e78cfdb0550
shauli <shauli321@gmail.com>
2022-06-02 15:42:25 +0200

    update

--- bios/run.sh
+++ bios/run.sh
@@ -4,14 +4,14 @@ python3 finetune.py --run_id 2 --device 2 --adv 0 --opt sgd &
 python3 finetune.py --run_id 3 --device 3 --adv 0 --opt sgd &
 python3 finetune.py --run_id 4 --device 0 --adv 0 --opt sgd
 
-python3 finetune.py --run_id 0 --device 0 --adv 1 --opt sgd
-python3 finetune.py --run_id 1 --device 0 --adv 1 --opt sgd
-python3 finetune.py --run_id 2 --device 0 --adv 1 --opt sgd
-python3 finetune.py --run_id 3 --device 0 --adv 1 --opt sgd
+python3 finetune.py --run_id 0 --device 0 --adv 1 --opt sgd &
+python3 finetune.py --run_id 1 --device 0 --adv 1 --opt sgd &
+python3 finetune.py --run_id 2 --device 0 --adv 1 --opt sgd &
+python3 finetune.py --run_id 3 --device 0 --adv 1 --opt sgd &
 python3 finetune.py --run_id 4 --device 0 --adv 1 --opt sgd
 
-python3 finetune.py --run_id 0 --device 0 --adv 1 --mlp_adv 1 --opt sgd
-python3 finetune.py --run_id 1 --device 0 --adv 1 --mlp_adv 1 --opt sgd
-python3 finetune.py --run_id 2 --device 0 --adv 1 --mlp_adv 1 --opt sgd
-python3 finetune.py --run_id 3 --device 0 --adv 1 --mlp_adv 1 --opt sgd
+python3 finetune.py --run_id 0 --device 0 --adv 1 --mlp_adv 1 --opt sgd &
+python3 finetune.py --run_id 1 --device 0 --adv 1 --mlp_adv 1 --opt sgd &
+python3 finetune.py --run_id 2 --device 0 --adv 1 --mlp_adv 1 --opt sgd &
+python3 finetune.py --run_id 3 --device 0 --adv 1 --mlp_adv 1 --opt sgd &
 python3 finetune.py --run_id 4 --device 0 --adv 1 --mlp_adv 1 --opt sgd
